0d277eb5db98451275673233484e8e834c6a29dc
pedro josue vidal moreno <josue_vid@hotmail.com>
2023-06-27 08:42:39 -0700

    inicial

---
+++ src/main/java/com/example/catalogocoppelsb/CatalogoCoppelSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogocoppelsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoCoppelSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoCoppelSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catalogocoppelsb/CatalogoCoppelSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogocoppelsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoCoppelSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
